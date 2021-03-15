029deb39bebc1f1abf54c5589161d4761ddf7741
wjcutrone <73838888+wjcutrone@users.noreply.github.com>
2021-03-15 11:42:23 -0400

    updates
    modifications to app.py and index.html

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -11,16 +11,18 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 @app.route("/")
 def home():
     #find one record of data from the mongo database
-    mars = mongo.db.mars_facts.find_one()
+    mars_facts = mongo.db.mars_facts.find_one()
 
     #return the template
-    return render_template("index.html", mars = mars)
+    return render_template("index.html", mars_facts = mars_facts)
 
 
 @app.route("/scrape")
 def scraper():
+    #set collection
+    mars_facts = mongo.db.mars_facts
 
-    #run the scrape function
+    #run the scrape function on scrape_mars.py
     mars_data = scrape_mars.scrape()
 
     #Update the Mongo database using update and upsert=True
